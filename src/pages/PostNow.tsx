ace8a3953d9eb77bd08b809cbcddfbc12b7de2e8
Anshul Mokhale <89470956+Anshul-Mokhale@users.noreply.github.com>
2024-06-17 11:53:35 +0530

    update 19

--- src/pages/PostNow.tsx
+++ src/pages/PostNow.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import DefaultLayout from "../layout/DefaultLayout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Modal from 'react-modal';
 import Cropper from 'cropperjs';
 import 'cropperjs/dist/cropper.css';
@@ -103,8 +103,8 @@ const PostNow: React.FC = () => {
     const [category, setCategory] = useState<string>(''); // You might want to add category options
     const [errorMessage, setErrorMessage] = useState<string>('');
     const fileInputRef = useRef<HTMLInputElement | null>(null);
-    const [msg, setMsg] = useState<string>('');
     const { createPost } = usePost();
+    const navigate = useNavigate();
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
@@ -165,7 +165,8 @@ const PostNow: React.FC = () => {
         } else {
             // handle successful post creation (e.g., redirect to the posts list, reset the form, etc.)
             console.log("Post created successfully:", response);
-            setMsg('Post created successfully');
+            localStorage.setItem('action', 'success');
+            navigate('/user/dashboard');
         }
     };
 
@@ -197,32 +198,7 @@ const PostNow: React.FC = () => {
                         </h3>
                     </div>
 
-                    {msg ? (
-                        <div className="flex w-full border-l-6 border-[#34D399] bg-[#34D399] bg-opacity-[15%] px-7 py-8 shadow-md dark:bg-[#1B1B24] dark:bg-opacity-30 md:p-9">
-                            <div className="mr-5 flex h-9 w-full max-w-[36px] items-center justify-center rounded-lg bg-[#34D399]">
-                                <svg
-                                    width="16"
-                                    height="12"
-                                    viewBox="0 0 16 12"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M15.2984 0.826822L15.2868 0.811827L15.2741 0.797751C14.9173 0.401867 14.3238 0.400754 13.9657 0.794406L5.91888 9.45376L2.05667 5.2868C1.69856 4.89287 1.10487 4.89389 0.747996 5.28987C0.417335 5.65675 0.417335 6.22337 0.747996 6.59026L0.747959 6.59029L0.752701 6.59541L4.86742 11.0348C5.14445 11.3405 5.52858 11.5 5.89581 11.5C6.29242 11.5 6.65178 11.3355 6.92401 11.035L15.2162 2.11161C15.5833 1.74452 15.576 1.18615 15.2984 0.826822Z"
-                                        fill="white"
-                                        stroke="white"
-                                    ></path>
-                                </svg>
-                            </div>
-                            <div className="w-full">
-                                <h5 className="mb-3 text-lg font-semibold text-black dark:text-[#34D399] ">
-                                    {msg}
-                                </h5>
-                            </div>
-                        </div>
-                    ) : (
-                        ''
-                    )}
+
                     <form onSubmit={handleSubmit}>
                         <div className="p-6.5">
                             <div className="mb-4.5">
