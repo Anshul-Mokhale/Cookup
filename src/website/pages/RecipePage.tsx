f180faeebe76846409d80a94ac0f7aa763a52576
Anshul Mokhale <89470956+Anshul-Mokhale@users.noreply.github.com>
2024-06-18 17:20:33 +0530

    test commit 3

--- src/website/pages/RecipePage.tsx
+++ src/website/pages/RecipePage.tsx
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { usePost } from "../../context/PostContext";
 import Layout from "../Layout";
+import Loader from "../../common/Loader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-regular-svg-icons";
 
 const RecipePage: React.FC = () => {
     const { id } = useParams<{ id: string }>();
     const { viewPost } = usePost();
     const [recipe, setRecipe] = useState<any>(null);
-    // const [loading, setLoading] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchRecipe = async () => {
             if (!id) {
                 setError("Recipe ID is required.");
-                // setLoading(false);
+                setLoading(false);
                 return;
             }
             try {
@@ -27,24 +30,24 @@ const RecipePage: React.FC = () => {
             } catch (error: any) {
                 setError(error.message || "An error occurred during fetching data.");
             } finally {
-                // setLoading(false);
+                setLoading(false);
             }
         };
 
         fetchRecipe();
     }, [id, viewPost]);
 
-    // if (loading) {
-    //     return <div>Loading...</div>;
-    // }
+    if (loading) {
+        return <div><Loader /></div>;
+    }
 
     if (error) {
         return <div>Error: {error}</div>;
     }
 
-    if (!recipe) {
-        return <div>No recipe found.</div>;
-    }
+    // if (!recipe) {
+    //     return <div>No recipe found.</div>;
+    // }
 
     // Function to convert newline characters to <br> tags
     const formatText = (text: string) => {
@@ -65,12 +68,18 @@ const RecipePage: React.FC = () => {
                             <img src={recipe.recipeImage} alt={recipe.title} />
                             <h1 className="text-center font-bold text-4xl">{recipe.title}</h1>
                         </div>
-                        <h1 className="text-black dark:text-white font-bold text-2xl mb-2">Description</h1>
-                        <p className="mb-4">{formatText(recipe.description)}</p>
-                        <h1 className="text-black dark:text-white font-bold text-2xl mb-2">Ingredients</h1>
-                        <p className="mb-4">{formatText(recipe.ingredient)}</p>
-                        <h1 className="text-black dark:text-white font-bold text-2xl mb-2">Steps</h1>
-                        <p className="mb-4">{formatText(recipe.steps)}</p>
+                        <div className="flex flex-row items-center justify-between">
+                            <p>Date:{new Date(recipe.createdAt).toLocaleDateString()}</p>
+                            <button><FontAwesomeIcon icon={faBookmark} />&nbsp; Save</button>
+                        </div>
+                        <div className="py-4">
+                            <h1 className="text-black dark:text-white font-bold text-2xl mb-2">Description</h1>
+                            <p className="mb-4">{formatText(recipe.description)}</p>
+                            <h1 className="text-black dark:text-white font-bold text-2xl mb-2">Ingredients</h1>
+                            <p className="mb-4">{formatText(recipe.ingredient)}</p>
+                            <h1 className="text-black dark:text-white font-bold text-2xl mb-2">Steps</h1>
+                            <p className="mb-4">{formatText(recipe.steps)}</p>
+                        </div>
                     </div>
                 </div>
             </Layout>
