cdc67d98d4b3ba106dd3fc369d02369019f3d7c1
Anshul Mokhale <89470956+Anshul-Mokhale@users.noreply.github.com>
2024-06-19 19:15:22 +0530

    nrdty

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -25,7 +25,7 @@ const Profile = () => {
         <div className="px-4 pb-6 text-center lg:pb-8 xl:pb-11.5">
           <div className="relative z-30 mx-auto -mt-22 h-30 w-full max-w-30 rounded-full bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
             <div className="relative drop-shadow-2 ">
-              <img src={parsedUser.avatar} alt="profile" className='customradius' />
+              <img src={parsedUser.avatar} alt="profile" style={{ borderRadius: "50%" }} />
             </div>
           </div>
           <div className="mt-4">
