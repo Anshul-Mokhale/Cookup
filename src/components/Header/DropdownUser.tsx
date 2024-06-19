1a6ba682045cc2197cbad55e2ad47ce12629e255
Anshul Mokhale <89470956+Anshul-Mokhale@users.noreply.github.com>
2024-06-19 19:19:00 +0530

    final update 2

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -61,7 +61,6 @@ const DropdownUser = () => {
           <span className="block text-sm font-medium text-black dark:text-white">
             {parsedUser.name}
           </span>
-          <span className="block text-xs">UX Designer</span>
         </span>
 
         <span className="h-12 w-12 rounded-full">
